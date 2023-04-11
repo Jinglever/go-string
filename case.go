c9b3921cb9734af2fde3487049d8849547016df0
howard <zhihao.zhou.bupt@gmail.com>
2023-04-11 13:04:25 +0800

    add func Lcfirst

--- case.go
+++ case.go
@@ -2,10 +2,18 @@ package jgstr
 
 import "strings"
 
-// 首字母大写
+// convert first letter to upper case
 func Ucfirst(s string) string {
 	if s == "" {
 		return ""
 	}
 	return strings.ToUpper(s[0:1]) + s[1:]
+}
+
+// convert first letter to lower case
+func Lcfirst(s string) string {
+	if s == "" {
+		return ""
+	}
+	return strings.ToLower(s[0:1]) + s[1:]
 }
